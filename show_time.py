34b2dfd1d409d5c8985e4da5169db0b8b00a06cd
rohitsharma0304 <sharma.rohi@northeastern.edu>
2022-07-02 12:00:28 -0700

    date and time

--- show_time.py
+++ show_time.py
@@ -3,3 +3,5 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time = ", current_time)
+today = now.strftime("%d/%m/%y")
+print("Today's Date = ", today)
